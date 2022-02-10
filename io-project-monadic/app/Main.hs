9265d7ad3a28d7404437d30fa7471dbc64bb284d
Philip Jacob <philipj@versafile.com>
2022-02-09 22:59:50 -0800

    Added RunApp

--- io-project-monadic/app/Main.hs
+++ io-project-monadic/app/Main.hs
@@ -1,30 +1,25 @@
 module Main where
 import Control.Monad.Trans.Writer (WriterT,runWriterT)
-import qualified Data.Text as T
+
 import Control.Monad.State
 import Control.Monad.Reader
 
-import Animation.Animate (animate)
+import Animation.Animate (animate, runApp)
 import Animation.CLI (cli)
 import Control.Monad.State (evalStateT)
-
---import Animation.Animate (animate)
+import Control.Monad.Reader (MonadReader(ask))
+import qualified Data.Text as T
 --import Animation.CLI (cli)
 
 main :: IO ()
-
-
---type AppM a = WriterT T.Text (StateT s (ReaderT env IO)) a
---runApp :: AppM () -> IO T.Text
-
---main = cli >>= (\(env, state) -> animate env state)
-
 main = do
   -- do some cli parsing
-    (env, state) <- cli
-    --runReaderT (runStateT animate state) env
-    x <-runReaderT (runStateT (runWriterT animate) state) env
-    return ()
-  --  animate env
-  -- logs <- runApp animationApp
-  -- write the logs to a file called log.txt
+  (env, state) <- cli
+  writeFile "bouncingball.txt" "Starting ball bounce"
+
+  --x <-runReaderT (runStateT (runWriterT animate) state) env
+  --writeFile "~/bouncingball.txt" (T.unpack $ snd (fst x))
+
+  y <- runApp animate env state
+  writeFile "bouncingball.txt" (T.unpack y)
+   
